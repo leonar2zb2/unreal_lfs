f6dd106e3318be7f617484e687c31d39c3bf6c07
usuario 2 <uer2@gmail.com>
2025-03-13 21:53:57 -0300

    + Implementado en el módulo C++ Iniciar el juego + Implementado Revelar palabra + Implementado probar letra + Agregada documentación a los métodos para Doxygen + Agregado nodos y flujo con lógica para demo juego

--- Source/UtilModule/Private/Palabras.cpp
+++ Source/UtilModule/Private/Palabras.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Palabras.h"
+#include "Math/UnrealMathUtility.h"
 
 int UPalabras::Agregar(FString cadena)
 {
@@ -18,4 +19,52 @@ void UPalabras::Listar()
 {
     for (FString palabra : Almacen)
         UE_LOG(LogTemp, Warning, TEXT("Palabra %s"), *palabra);
+}
+
+FString UPalabras::Iniciar(int CuantosIntentos)
+{
+    int32 RandomValue = -1;
+    Seleccionada = "";
+    Respuesta = "";
+    IntentosRestantes = CuantosIntentos;
+    MaxIntentos = CuantosIntentos;
+    if (Almacen.Num() > 0)
+    {
+        RandomValue = FMath::RandRange(0, Almacen.Num() - 1);
+        Seleccionada = Almacen[RandomValue];
+    }
+    for (char letra : Seleccionada)
+        Respuesta += '_';
+    UE_LOG(LogTemp, Warning, TEXT("Random Value: %d"), RandomValue);
+    UE_LOG(LogTemp, Warning, TEXT("Palabra %s"), *Seleccionada);
+    return Respuesta;
+}
+
+bool UPalabras::ProbarConLetra(FString letra, FString &Resultado, int &IntentosPendientes)
+{
+    bool Presente = false;
+    TCHAR caracter;
+    if (!letra.IsEmpty()) // Verificar que la cadena no esté vacía
+    {
+        caracter = letra[0]; // Tomar el primer carácter
+        for (int i = 0; i < Seleccionada.Len(); i++)
+            if (Seleccionada[i] == caracter)
+            {
+                Presente = true;
+                Respuesta[i] = caracter;
+            }
+    }
+    if (!Presente)
+        IntentosRestantes--;
+    IntentosPendientes = IntentosRestantes;
+    Resultado = Respuesta;
+    return Presente;
+}
+
+FString UPalabras::Revelar(bool &Revelado)
+{
+    Revelado = IntentosRestantes <= 0;
+    if (Revelado)
+        return Seleccionada;
+    return FString::ChrN(Seleccionada.Len(), TEXT('*'));
 }
--- Source/UtilModule/Public/Palabras.h
+++ Source/UtilModule/Public/Palabras.h
@@ -14,15 +14,59 @@ class UTILMODULE_API UPalabras : public UObject
 {
 	GENERATED_BODY()
 public:
+	/**
+	 * Permite agregar una cadena de texto a las palabras seleccionadas a adivinar
+	 * @param cadena La cadena que se desea agregar
+	 * @return Cantidad de palabras de la lista a adivinar
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Palabras")
 	int Agregar(FString cadena);
 
+	/**
+	 * Agrega en un paso un conjunto de palabras candidatas a adivinar
+	 * @param cadenas Arreglo de cadenas a agregar
+	 * @return Devuelve la cantidad de palabras totales
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Palabras")
 	int AgregarMultiples(TArray<FString> cadenas);
 
+	/**
+	 * Lista todas las palabras agregadas
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Palabras")
 	void Listar();
 
+	/**
+	 * Inicia, seleccionando arbitrariamente una palabra de las agregadas
+	 * @param CuantosIntentos Define la cantidad de veces a errar admisibles
+	 * @return Regresa una cadena formateada con guiones bajos, uno por cada letra de la palabra seleccionada
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Palabras")
+	FString Iniciar(int CuantosIntentos = 5);
+
+	/**
+	 * Verifica si una letra está incluida en la palabra secreta
+	 * @param letra Letra a intentar buscar
+	 * @param Resultado Cadena con las letras ya sustituidas y acumuladas
+	 * @param IntentosPendientes Devuelve la cantidad de intentos por realizar
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Palabras")
+	bool ProbarConLetra(FString letra, FString &Resultado, int &IntentosPendientes);
+
+	/**
+	 * Devuelve la cadena secreta pero sólo si se agotaron los intentos previstos
+	 *
+	 * @param Revelado Verdadero si fue revelada, falso en caso contrario
+	 * @return Cadena seleccionada o varios asteriscos(*) cuando no debe
+	 *
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Palabras")
+	FString Revelar(bool &Revelado);
+
 private:
 	TArray<FString> Almacen;
+	FString Seleccionada;
+	FString Respuesta;
+	int MaxIntentos;
+	int IntentosRestantes;
 };
